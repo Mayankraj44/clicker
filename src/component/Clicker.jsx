20b838c9ab9436c0994753167b9472e8ac4e46bf
Mayank Raj <lui.chang420@gmail.com>
2022-12-14 23:17:46 +0530

    structure complete

---
+++ src/component/Clicker.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Clicker() {
+  function handleUndo() {}
+  function handleRedo() {}
+  return (
+    <>
+      <button>Undo</button>
+      <button>Redo</button>
+      <div></div>
+    </>
+  );
+}
+
+export default Clicker;
